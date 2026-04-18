45f2e6c7cbfa80a0fd5e48a94028ff4a5d9eaa7c
Jass <jswasal@gmail.com>
2026-04-17 22:02:48 -0400

    Added swagger documentation and removed openAPI docs

--- OutageApi.Web/Program.cs
+++ OutageApi.Web/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-
+builder.Services.AddSwaggerGen();
 
 //Inject dependencies
 builder.Services.AddSingleton<IOutageRepository, InMemoryOutageRepository>();
@@ -20,7 +20,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
